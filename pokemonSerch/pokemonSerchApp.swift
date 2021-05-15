156ba7ceadfb0e834449e9a3597de4f64920de52
葛 智紀 <tomoki.katsu@kddi-web.com>
2021-05-15 16:48:17 +0900

    Initial Commit

---
+++ pokemonSerch/pokemonSerchApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonSerchApp.swift
+//  pokemonSerch
+//
+//  Created by 葛 智紀 on 2021/05/15.
+//
+
+import SwiftUI
+
+@main
+struct pokemonSerchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
